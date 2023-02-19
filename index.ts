572730866d7e7e32bf0a282993488aa82df93b70
Georges DIMITRY <georges.dimitry@hotjar.com>
2023-02-20 00:36:51 +0200

    Refactor

--- index.ts
+++ index.ts
@@ -1,23 +1,38 @@
-type NestedKeysValuesOf<T, B extends string='', Keys = keyof T> =
+import get from 'lodash/get';
+
+type UnionToIntersection<U> = 
+  (U extends any ? (k: U)=>void : never) extends ((k: infer I)=>void) ? I : never;
+
+type NestedKeysValuesOf<T, BasePathT extends string='', Keys = keyof T> =
 T extends Array<infer U>
-    ? B extends ''
+    ? BasePathT extends ''
         ? Record<`[${number}]`, U> & NestedKeysValuesOf<T[number], `[${number}]`>
-        : Record<`${B}[${number}]`, U> & NestedKeysValuesOf<T[number], `${B}[${number}]`>
+        : Record<`${BasePathT}[${number}]`, U> & NestedKeysValuesOf<T[number], `${BasePathT}[${number}]`>
     : T extends {[Keys: string | number]: infer V}
         ? Keys extends string | number
-            ? B extends ''
+            ? BasePathT extends ''
                 ? Record<`${Keys}`, T[Keys]> & NestedKeysValuesOf<T[Keys], `${Keys}`>
-                : Record<`${B}.${Keys}`, T[Keys]> & NestedKeysValuesOf<T[Keys], `${B}.${Keys}`>
+                : Record<`${BasePathT}.${Keys}`, T[Keys]> & NestedKeysValuesOf<T[Keys], `${BasePathT}.${Keys}`>
             : unknown
         :unknown;
 
+type NestedKeyTypeMap<T, B extends string='', Keys = keyof T> = UnionToIntersection<NestedKeysValuesOf<T, B, Keys>>;
+
 function getIn<
     T,
-    KeyValueMap extends NestedKeysValuesOf<T>,
+    KeyValueMap extends NestedKeyTypeMap<T>,
     Path extends keyof KeyValueMap,
 >(object: T, path: Path): KeyValueMap[Path] {
-
+    return get(object, path);
 }
 
-const targetObj = {c: {b: [1,2,3]}};
+const targetObj = {
+    a: 1,
+    c: {
+        a: 1,
+        c: 2,
+        b: [1,2,3]
+    },
+};
 const final = getIn(targetObj, 'c.b[1]');
+console.log(final);
