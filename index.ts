3208d9fcd83f16c648fdf2d8514b68d22f496d90
Georges DIMITRY <georges.dimitry@hotjar.com>
2023-02-20 04:27:46 +0200

    Add support to readonly arrays

--- index.ts
+++ index.ts
@@ -3,18 +3,28 @@ import get from 'lodash/get';
 type UnionToIntersection<U> = 
   (U extends any ? (k: U)=>void : never) extends ((k: infer I)=>void) ? I : never;
 
-type NestedKeysValuesOf<T, BasePathT extends string='', Keys = keyof T> =
-T extends Array<infer U>
-    ? BasePathT extends ''
-        ? Record<`[${number}]`, U> & NestedKeysValuesOf<T[number], `[${number}]`>
-        : Record<`${BasePathT}[${number}]`, U> & NestedKeysValuesOf<T[number], `${BasePathT}[${number}]`>
-    : T extends {[Keys: string | number]: infer V}
-        ? Keys extends string | number
-            ? BasePathT extends ''
-                ? Record<`${Keys}`, T[Keys]> & NestedKeysValuesOf<T[Keys], `${Keys}`>
-                : Record<`${BasePathT}.${Keys}`, T[Keys]> & NestedKeysValuesOf<T[Keys], `${BasePathT}.${Keys}`>
-            : unknown
-        :unknown;
+type NestedKeysValuesOf<
+    T,
+    BasePathT extends string='',
+    Keys = T extends readonly any[] ? Extract<keyof T, `${number}`> : keyof T,
+> =
+    T extends Array<infer U>
+        ? BasePathT extends ''
+            ? Record<`[${number}]`, U> & NestedKeysValuesOf<T[number], `[${number}]`>
+            : Record<`${BasePathT}[${number}]`, U> & NestedKeysValuesOf<T[number], `${BasePathT}[${number}]`>
+        : T extends {[Keys: string | number]: infer V}
+            ? Keys extends string | number
+                ? BasePathT extends ''
+                    ? Record<`${Keys}`, T[Keys]> & NestedKeysValuesOf<T[Keys], `${Keys}`>
+                    : Record<`${BasePathT}.${Keys}`, T[Keys]> & NestedKeysValuesOf<T[Keys], `${BasePathT}.${Keys}`>
+                : unknown
+            : T extends readonly any[]
+                ? Keys extends (string | number) & keyof T
+                    ? BasePathT extends ''
+                        ? Record<`[${Keys}]`, T[Keys]> & NestedKeysValuesOf<T[Keys], `[${Keys}]`>
+                        : Record<`${BasePathT}[${Keys}]`, T[Keys]> & NestedKeysValuesOf<T[Keys], `${BasePathT}[${Keys}]`>
+                    : unknown
+                : unknown;
 
 type NestedKeyTypeMap<T, B extends string='', Keys = keyof T> = UnionToIntersection<NestedKeysValuesOf<T, B, Keys>>;
 
@@ -30,9 +40,9 @@ const targetObj = {
     a: 1,
     c: {
         a: 1,
-        c: 2,
-        b: [1,2,3]
+        b: ['1',2,3] as const,
+        c: [1,2,3],
     },
 };
-const final = getIn(targetObj, 'c.b[1]');
+const final = getIn(targetObj, 'c.c[0]');
 console.log(final);
